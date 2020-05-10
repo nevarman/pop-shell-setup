b45bc4adeeb84e0566b2150bcc7cd8ecf89e0180
Nevzat Arman <nevzatarman@gmail.com>
2020-05-10 14:31:20 +0200

    fix disable

--- extensions/hidepopshellbutton@nevarman.github.com/extension.js
+++ extensions/hidepopshellbutton@nevarman.github.com/extension.js
@@ -6,10 +6,10 @@ let activitiesButton;
 
 function enable() {
     // added a bit delay, I guess pop loading takes time and there is no execution order for extensions
-    Mainloop.timeout_add_seconds(2, Lang.bind(this, this.hide));
+    Mainloop.timeout_add_seconds(0.5, Lang.bind(this, this.hide));
 }
-async function hide(){
- activitiesButton = await Main.panel.statusArea['pop-shell'];
+function hide(){
+ activitiesButton = Main.panel.statusArea['pop-shell'];
     if(activitiesButton != null)
         activitiesButton.container.hide();
 
